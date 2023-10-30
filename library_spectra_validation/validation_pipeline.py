253c327b255d682bb8e48fe352173338bf72a315
niekdejonge <niekdejonge@gmail.com>
2023-10-30 18:12:00 +0100

    First initialisation of files.

---
+++ library_spectra_validation/validation_pipeline.py
@@ -0,0 +1,7 @@
+"""Code for running the validation pipeline.
+Includes repairing and validating.
+Repairs should be documented in a computer readable way
+
+Require correct metadata filters output None if the requirement is not passed, we also need to store which filter was not passed.
+Repair functions, just change the metadata, so we can check for changes that occured.
+"""
