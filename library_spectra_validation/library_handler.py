49312cfe7a99ab2f4f11ef92580841d1501d00c3
omokshyna <mokshinaelena@ukr.net>
2023-10-31 12:21:28 +0100

    Started adding functionality to LibraryHandler

--- library_spectra_validation/library_handler.py
+++ library_spectra_validation/library_handler.py
@@ -1,5 +1,26 @@
-
+from matchms.importing import load_spectra
 
 class LibraryHandler:
     """Stores the 3 different types of spetra. Correct, repaired, wrong.
     Has internal organization using spectrum ids"""
+
+    def __init__(self, f, pipeline):
+        self.spectra = load_spectra(f)
+        self.pipeline = pipeline
+        
+        self.spectra_dictionary = {
+            'valid': None, #[id1, id2,...]
+            'repaired': None, #[id1:[modifications],..]
+            'invalid': None #also a dictionary
+        }
+        self.modifications = []
+
+    def clean_and_validate_spectrum(spectrum_id):
+        spectrum = self.spectra[spectrum_id]
+        modifications = self.pipeline.run(spectrum)
+        self.spectra_dictionary[modifications.spectra_quality].append(spectrum_id) #vcalid, repired,...
+        self.modifications.append(modifications)
+
+    def run():
+        for spectrum in self.spectra:
+            clean_and_validate(spectrum)
--- library_spectra_validation/validation_pipeline.py
+++ library_spectra_validation/validation_pipeline.py
@@ -4,4 +4,15 @@ Repairs should be documented in a computer readable way
 
 Require correct metadata filters output None if the requirement is not passed, we also need to store which filter was not passed.
 Repair functions, just change the metadata, so we can check for changes that occured.
+
+Add index handling - when a spectrum is processed, its id is added to the corresponding list
 """
+
+class ValidationPipeline:
+
+    def __init__(self, spectrum) -> None:
+        pass
+
+    def run(spectrum):
+        #here run all the checks and modifications
+        return instructions #{'quality':, 'metadata adduct': {'previous':..., 'updated':....}}
